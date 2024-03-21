21d35e780a32d29b6d2ddfcad8c5e567650285cd
Shy <shy95419@gmail.com>
2024-03-22 08:39:47 +0900

    1

--- 10869 사칙연산.py
+++ 10869 사칙연산.py
@@ -1,6 +1,7 @@
-# A,B = map(int, input().split())
-# print(A+B, A-B, A*B, A//B, A%B, sep='\n')
-# # sep='\n'로 줄바꿈
+A,B = map(int, input().split())
+print(A+B, A-B, A*B, A//B, A%B, sep='\n')
+# sep='\n'로 줄바꿈
+
 
 
 myList = {1,2,3,4,5}
