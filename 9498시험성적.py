8bbaba021abc20279fb598e285b465eee0fea4e4
Shy <shy95419@gmail.com>
2024-03-22 09:00:44 +0900

    day1

--- 9498시험성적.py
+++ 9498시험성적.py
@@ -1,7 +1,7 @@
 
-score = int(input())
+score = int(input()) # 시험 점수를 'score'라는 변수로 지정 'input()'함수로  숫자입력 숫자는 문자형이기 때문에 이를 'int()'로 정수로 변환
 
-if score >= 90:
+if score >= 90:     # 만약 'score'=입력한 숫자가 90보다 크거나 같다면 A를 출력
     print('A')
 elif score >= 80:
     print('B')
@@ -9,5 +9,5 @@ elif score >= 70:
     print('C')
 elif score >= 60:
     print('D')
-else:
+else:               # 위 모든 사항에 해당하지 않으면 F를 출력
     print('F')
