c4468d37b67ebd929784fd4c6110c2bfc1c0d424
Shy <shy95419@gmail.com>
2024-03-22 11:02:20 +0900

    day1

--- 2562최댓값.py
+++ 2562최댓값.py
@@ -1,10 +1,10 @@
-numbers = []    # 빈 리스트를 만든다. 9개의 정수가 담길 예정
+# numbers = []    # 빈 리스트를 만든다. 9개의 정수가 담길 예정
 
 # for _ in range(9):  # range함수를 사용해 횟수를 지정함.
 #     i = int(input())    # i라는 변수에 입력받은 숫자를 정수로 변환해 넣는다.
 #     numbers.append(i)   # 'numbers'라는 리스트에 정수로 변환된 i를 넣는다.
 
-numbers = [int(input()) for _ in range(9)]
+numbers = [int(input()) for _ in range(9)] # list comprehension 문법을 사용해 리스트를 쉽고 짧게 한 줄로 만듦
     
     
 print(max(numbers))     #'numbvers' 리스트 max함수를 사용해 최댓값을 찾는다.
