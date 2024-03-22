2fd465761cbd170ea3e0c0c9c9e7bf746671700e
Shy <shy95419@gmail.com>
2024-03-22 14:36:13 +0900

    day1

---
+++ 2675문자열 반복.py
@@ -0,0 +1,8 @@
+n = int(input())    # 아래 구문의 반복 횟수 정수로 입력받는다.
+
+for _ in range(n):  # 아래 구문을 반복한다. 
+    cnt,word = input().split()  # 단어의 반복 횟수와 단어를 띄어쓰기를 기준으로 입력받아 각각 cnt와 word 변수에 할당.
+    
+    for x in word:  # word를 반복해 x에 넣는다. 
+        print(x*int(cnt), end='')   # 들어온 단어 x를 cnt만큼 반복하고 end=를 사용해 출력을 이어서 한다.
+    print()     # 줄바꿈
